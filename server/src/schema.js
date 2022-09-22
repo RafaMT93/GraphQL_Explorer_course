c85ae9b00a3fa9cc725bc8c75e9d26e01c26681f
Rafael Moraes <rafael.moraes@ilog.com.br>
2022-09-22 12:48:47 -0300

    Basic of GraphQL Front and Backend

---
+++ server/src/schema.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    "Track group"
+    type Track {
+        id: ID
+        title: String!
+        author: Author!
+        thumbnail: String!
+        length: Int
+        modulesCount: Int
+    }
+    
+    "Author group"
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
+
+    type Query {
+        "Array of track for"
+        tracksForHome: [Track!]!
+    }
+`;
+
+module.exports = typeDefs;
